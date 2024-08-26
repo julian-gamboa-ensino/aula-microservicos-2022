dd321e879409330c47799e9272aa7f1b7a47e357
julian-gamboa-ensino <julian.gamboa.ms.2021@gmail.com>
2024-08-26 08:39:01 -0300

    Analisando API https://www.youtube.com/watch?v=u3GAYK8hvTI

--- micro-projetos-didaticos/main.go
+++ micro-projetos-didaticos/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
-	"agosto-24/circle"
-	"agosto-24/square"
+	"agosto-24/Areas/circle"
+	"agosto-24/Areas/square"
 	"fmt"
 )
 
-type Shape interface {
+type Shape2D interface {
 	Area() float64
 }
 
+type Shape3D interface {
+	Shape2D
+	Volume() float64
+}
+
 // A helper function to calculate the area of any Shape
-func calculateArea(s Shape) float64 {
+func calculateArea(s Shape2D) float64 {
 	return s.Area()
 }
 
